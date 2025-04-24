fff321c439d25edf28a5a34f6d297600b310ccbb
Sai Anumala <154539727+sai-anumala@users.noreply.github.com>
2025-04-24 17:22:38 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <meta name="copyright" content="MACode ID, https://macodeid.com/">
+
+  <title>About us</title>
+
+  <link rel="stylesheet" href="./assets/css/maicons.css">
+
+  <link rel="stylesheet" href="./assets/css/bootstrap.css">
+
+  <link rel="stylesheet" href="./assets/vendor/owl-carousel/css/owl.carousel.css">
+
+  <link rel="stylesheet" href="./assets/css/theme.css">
+</head>
+<body>
+
+ <div class="back-to-top"></div>
+
+  <header>
+    <img src="./assets/img/logo.png" class="logo">
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+      <div class="container">
+        <form action="#">
+          <div class="input-group input-navbar">
+          </div>
+        </form>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupport">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="vaccines.php">Vaccine Information</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog.php">Events</a>
+            </li>
+          </ul>
+        </div> 
+      </div> 
+    </nav>
+  </header>
+  <div class="page-banner overlay-dark bg-image" style="background-image: url(./assets/img/123.jpg);">
+    <div class="banner-section">
+      <div class="container text-center wow fadeInUp">
+        <h1 class="font-weight-normal">About us</h1>
+      </div>
+    </div> 
+  </div> 
+
+  <div class="page-section">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 wow fadeInUp">
+          <h1 class="text-center mb-3">Welcome to XYZ Health Care</h1>
+          <div class="text-lg">
+            <p allign="justify">XYZ Health Care presents ‘SafeVax’ — A complete online system that makes managing vaccinations easier. Instead of standing in long queues, patients can now conveniently book their vaccination appointments online. The system efficiently manages scheduling, vaccine inventory, user registration, and progress tracking through a centralized platform. With role-based dashboards for admins, vaccinators, and patients, and automated email confirmations and reminders, ‘SafeVax’ reduces waiting time by 15–20 minutes and eases administrative strain. The system also supports real-time campaign updates and secure patient data management</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<script src="./assets/js/jquery-3.5.1.min.js"></script>
+
+<script src="./assets/js/bootstrap.bundle.min.js"></script>
+
+<script src="./assets/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+
+<script src="./assets/vendor/wow/wow.min.js"></script>
+
+<script src="./assets/js/theme.js"></script>
+  
+</body>
+</html>
---
+++ admin/edit_patient.php
@@ -0,0 +1,123 @@
+<?php
+include('authentication.php');
+include('includes/header.php');
+include('../message.php');
+?>
+<div class="container-fluid">
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+    <h4 class="m-0 font-weight-bold text-black" style="color:black;">Edit Details</h4> 
+</div>
+    <div class="card-body">
+        <?php
+            
+            if(isset($_POST['edit']))
+            {
+                $id=mysqli_real_escape_string($con,$_POST['edit_id']);
+                $query_run = mysqli_query($con,"SELECT * FROM user WHERE u_id = $id;");
+
+                foreach($query_run as $row)
+                {
+                ?>
+
+                    <form action="edit_profilecon.php" method="POST">
+                    <input type="hidden" name="edit_id" value="<?php echo $row['u_id'];?>" >
+
+                        <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>First name</label>
+                                <input class="form-control" name="fname" type="text"value="<?php echo $row['u_firstname'];?>" placeholder="Enter your first name">
+                            </div>
+                        </div>
+                        <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Last name</label>
+                                <input class="form-control" name="lname"  type="text" value="<?php echo $row['u_lastname'];?>"placeholder="Enter your last name">
+                            </div>
+                        </div>
+                          <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Father's First name</label>
+                                <input class="form-control" name="ffname" type="text" value="<?php echo $row['u_ffirstname'];?>"placeholder="Enter your father's first name">
+                            </div>
+                          </div>
+                        <div class="row gx-3 mb-3">
+                          <div class="col-md-6">
+                                <label>Father's Last name</label>
+                                <input class="form-control" name="flname" type="text" value="<?php echo $row['u_flastname'];?>"placeholder="Enter your father's last name">
+                            </div>
+                        </div>
+                        <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Father's NID</label>
+                                <input class="form-control" name="fnid"type="text" value="<?php echo $row['u_fnid'];?>"placeholder="Enter your father's NID">
+                            </div>
+                        </div>
+                        <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Division</label>
+                                <select name="division" class="custom-select" >
+                                    <option value="" disabled selected hidden><?php echo $row['u_division'];?></option>                                  
+                                    <option value="Visakhapatnam" <?php if($row['u_division']=="Visakhapatnam"){echo "selected";} ?>>Visakhapatnam</option>
+                                    <option value="Vijayawada" <?php if($row['u_division']=="Vijayawada"){echo "selected";} ?>>Vijayawada</option>
+                                    <option value="Guntur" <?php if($row['u_division']=="Guntur"){echo "selected";} ?>>Guntur</option>
+                                    <option value="Kakinada" <?php if($row['u_division']=="Kakinada"){echo "selected";} ?>>Kakinada</option>
+                                    <option value="Rajahmundry" <?php if($row['u_division']=="Rajahmundry"){echo "selected";} ?>>Rajahmundry</option>
+                                    <option value="Tirupati" <?php if($row['u_division']=="Tirupati"){echo "selected";} ?>>Tirupati</option>
+                                    <option value="Nellore" <?php if($row['u_division']=="Nellore"){echo "selected";} ?>>Nellore</option>
+                                    <option value="Anantapur" <?php if($row['u_division']=="Anantapur"){echo "selected";} ?>>Anantapur</option>
+                                    <option value="Chittoor" <?php if($row['u_division']=="Chittoor"){echo "selected";} ?>>Chittoor</option>
+                                    <option value="Kadapa" <?php if($row['u_division']=="Kadapa"){echo "selected";} ?>>Kadapa</option>
+                                </select>
+                            </div>
+                            </div>
+                          <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Date of Birth</label>
+                                 <input placeholder="Enter Date of Birth" name="dob" type="date" value="<?php echo $row['u_dob'];?>" onfocus="(this.type='date')" class="form-control">
+                            </div>
+                </div>
+                            <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Gender</label>
+                                <select name="gender" class="custom-select">
+                                    <option value="" disabled selected hidden><?php echo $row['u_gender'];?></option>
+                                    <option value="Male" <?php if($row['u_gender']=="Male"){echo "selected";}?>>Male</option>
+                                    <option value="Female" <?php if($row['u_gender']=="Female"){echo "selected";}?>>Female</option>
+                                </select>
+
+                               </div>
+                          </div>
+                            <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input class="form-control" type="password" name="password" value="<?php echo $row['u_password'];?>"placeholder="Enter password">
+                            
+                          </div>
+                          </div>
+                        
+                          <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Confirm Password</label>
+                                <input class="form-control" name="rpassword" type="password" value="<?php echo $row['u_password'];?>" placeholder="Confirm new password" >
+                            </div>
+                        </div>
+                <div class="row gx-3 mb-3">
+                            <div class="col-md-6">
+                                <label>Email</label>
+                                <input class="form-control" type="email" name="email" value="<?php echo $row['u_email'];?>"placeholder="Enter email" readonly>
+                            </div>
+                </div>
+                        <button type="submit"class="btn btn-success" name="save" >Save changes</button>
+                    </form>
+                    <?php
+                }
+            }
+            ?>
+                </div>
+            </div>
+        </div>
+</div>
+<?php
+include('includes/scripts.php');
+?>
